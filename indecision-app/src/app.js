15037d3a863f4070fbd3c6e93e7300b2367a16c0
cohensharon <cohensharon1995@gmail.com>
2020-02-17 17:37:18 +0200

    setting up babel

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,15 +1,10 @@
-"use strict";
-
 console.log("app.js is working");
 
 //JSX = javascript XML
 //makes working with templates easier
 
-var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
-);
+var template = <h1>Indecision App</h1>;
 var appRoot = document.getElementById("app");
+
 
 ReactDOM.render(template, appRoot);
